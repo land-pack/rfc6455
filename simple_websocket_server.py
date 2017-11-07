dbccab1ac223d83d03b9958b116136145d89dcc8
land-pack <landpack@sina.com>
2017-11-07 16:07:07 +0000

    fixed websocket simple example

--- simple_websocket_server.py
+++ simple_websocket_server.py
@@ -5,7 +5,7 @@ import base64
 
 
 HOST = '127.0.0.1'
-PORT = 9888
+PORT = 9002
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,10 +13,17 @@ sock.bind(ADDR)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.listen(5)
 
-respone =  "HTTP/1.1 101 Switching Protocols\r\n"
-respone += "Upgrade: websocket\r\n"
-respone += "Connection: upgrade\r\n"
+#respone =  "HTTP/1.1 101 Switching Protocols\r\n"
+respone = "HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n"
+respone += "Upgrade: WebSocket\r\n"
+respone += "Connection: Upgrade\r\n"
 respone += "Sec-WebSocket-Accept: %s\r\n"
+
+respone = '\
+HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
+Upgrade: WebSocket\r\n\
+Connection: Upgrade\r\n\
+Sec-WebSocket-Accept: %s\r\n\r\n'
 
 def get_sec_key(data):
     lst = data.split("\r\n")
@@ -40,14 +47,19 @@ while True:
         except Exception as e:
             print (e)
             break
-        if not data:
-            break
+        if len(data) == 0:
+            continue
+        print 'recv from client>>', data
         param = get_sec_key(data)
-        client_sec = param.get("Sec-WebSocket-Key")
+        client_sec = param.get("Sec-WebSocket-Key").strip()
+        print '>>>%s<<<' % client_sec
+        client_sec += '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
         has = hashlib.sha1(client_sec)
         ss = has.digest()
         p = base64.b64encode(ss)
         respone = respone % p
+        print 'resquest', param
+        print '=' * 90
         print 'respone', respone
         #s = 'Hi, you said:{} at {}'.format(data, time.ctime())
         client_handler.send(respone)
